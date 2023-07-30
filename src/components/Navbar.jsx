c0f44fdbe731a675a40d4ea4f6bf40a02f6111f3
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-07-30 20:42:15 +0200

    starting navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+function Navbar() {
+  const [active, setActive] = useState('');
+
   return (
-    <div>Navbar</div>
-  )
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          className="flex items-center gap-2"
+          to={'/'}
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Adrian <span className="sm:block hidden">| JavaScript Mastery</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
