6368d964527e2be839757b37dcfc38b5ce2d051b
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-01 10:46:53 +0200

    about is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,58 @@
-import { Tilt } from 'react-tilt';
-import { motion } from 'framer-motion';
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
-function About() {
+function ServiceCard({ index, title, icon }) {
   return (
-    <section className={`${styles.paddingX}`}>
-      <motion.div>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          options={{ max: 45, scale: 1, speed: 450 }}
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="font-bold text-center text-white text-[20px]">
+            {title}
+          </h3>
+        </div>
       </motion.div>
-    </section>
+    </Tilt>
   );
 }
 
-export default About;
+function About() {
+  return (
+    <>
+      {/* text of Overview and Introduction */}
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+      {/* cards of you know */}
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default SectionWrapper(About, 'about');
