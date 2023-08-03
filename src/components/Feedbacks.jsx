f52118361ef29452af0b533d47dc79ac329fcd16
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-03 17:23:45 +0200

    feedback is done

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,6 +5,42 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
+function FeedbackCard({
+  index,
+  testimonial,
+  name,
+  image,
+  company,
+  designation,
+}) {
+  return (
+    <motion.div
+      variants={fadeIn("", "spring", index * 0.5, 0.75)}
+      className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+    >
+      <p className="text-white font-black text-[48px]">"</p>
+      <div className="mt-1">
+        <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+        <div className="mt-7 flex justify-between items-center gap-1">
+          <div className="flex-1 flex flex-col">
+            <p className="text-white font-medium text-[16px]">
+              <span className="blue-text-gradient">@</span> {name}
+            </p>
+            <p className="mt-1 text-secondary text-[12px]">
+              {designation} of {company}
+            </p>
+          </div>
+          <img
+            src={image}
+            alt={`feedback-by-${name}`}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
 function Feedbacks() {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
@@ -16,8 +52,13 @@ function Feedbacks() {
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard index={index} key={testimonial.name} {...testimonial} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default Feedbacks;
+export default SectionWrapper(Feedbacks, '');
