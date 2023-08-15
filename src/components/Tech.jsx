c6705a9529e371112b4bec56332f12f859043d54
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-15 13:25:18 +0200

    tech balls none on small screens

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,39 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Tech() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div key={technology.name} className="w-28 h-28">
-          <BallCanvas icon={technology.icon} />
+    <>
+      {windowWidth > 650 ? (
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+          {technologies.map((technology) => (
+            <div key={technology.name} className="w-28 h-28">
+              <BallCanvas icon={technology.icon} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+          {technologies.map((technology) => (
+            <div key={technology.name} className="w-28 h-28">
+              <img src={technology.icon} alt={technology.name} />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
