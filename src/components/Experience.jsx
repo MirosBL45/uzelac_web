44bd6c92f3fbbd18e98a722772594c1ad0ec59e2
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-08-17 16:37:42 +0200

    started expirience language

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,26 +1,32 @@
-import { VerticalTimeline } from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
+import { VerticalTimeline } from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
 
-import "react-vertical-timeline-component/style.min.css";
+import 'react-vertical-timeline-component/style.min.css';
 
-import { styles } from "../styles";
-import { experiences, experiencesSR } from "../constants";
-import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
-import ExpirienceCard from "./parts_of_components/ExpirienceCard";
+import { styles } from '../styles';
+import { experiences, experiencesSR } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
+import ExpirienceCard from './parts_of_components/ExpirienceCard';
 
 function Experience({ language }) {
   return (
     <>
       {/* headlines */}
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <p className={styles.sectionSubText}>
+          {language === 'eng'
+            ? 'What I have done so far'
+            : 'Ono što sam do sada uradio'}
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          {language === 'eng' ? 'Work Experience' : 'Mojih Ruku Delo'}.
+        </h2>
       </motion.div>
       {/* cards of experiences, ExpirienceCard is imported up */}
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
-          {language === "eng"
+          {language === 'eng'
             ? experiences
                 .slice()
                 .reverse()
@@ -39,4 +45,4 @@ function Experience({ language }) {
   );
 }
 
-export default SectionWrapper(Experience, "work");
+export default SectionWrapper(Experience, 'work');
