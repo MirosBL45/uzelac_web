11f646f5345b0d05e1f261c7b3a2e4cb4380752e
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-01-16 21:24:59 +0100

    all works no motion

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -11,24 +11,28 @@ function Works({ language }) {
   return (
     <>
       {/* headlines */}
-      <motion.div variants={textVariant()}>
+      {/* <motion.div variants={textVariant()}> */}
+      <div>
         <p className={styles.sectionSubText}>
           {language === 'eng' ? 'My work' : 'Moj rad'}
         </p>
         <h2 className={styles.sectionHeadText}>
           {language === 'eng' ? 'Projects' : 'Projekti'}.
         </h2>
-      </motion.div>
+        {/* </motion.div> */}
+      </div>
       {/* description of all work */}
       <div className="w-full flex">
-        <motion.p
+        {/* <motion.p
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
           variants={fadeIn('', '', 0.1, 1)}
-        >
+        > */}
+        <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
           {language === 'eng'
             ? 'My portfolio includes diverse projects, from iconic concerts to film productions that have entered the hearts of viewers. With my ability to harmonize light, colors and sound, I provide depth and dimension to any environment. My experience in sound further enriches each production, laying the foundation for the total immersion of the audience in the event.'
             : 'Moj portfolio obuhvata raznolike projekte, od ikoničnih koncerata do filmskih ostvarenja koja su se uvukla u srca gledalaca. Svojom sposobnošću da uskladim svetlost, boje i zvuk, pružam dubinu i dimenziju svakom ambijentu. Moje iskustvo sa zvukom dodatno obogaćuje svaku produkciju, postavljajući temelje za potpuno uranjanje publike u događaj.'}
-        </motion.p>
+        </p>
+        {/* </motion.p> */}
       </div>
       {/* cards of work */}
       <div className="mt-20 flex flex-wrap gap-7">
